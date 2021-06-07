11b4da296c0bc6af5756daf246c7d4d379bf14d5
santoshakil office <santoshakil-office@gmail.com>
2021-06-07 15:23:12 +0600

    new data

--- bin/read_text_from_file.dart
+++ bin/read_text_from_file.dart
@@ -2,21 +2,19 @@ import 'dart:io';
 import 'dart:convert';
 
 Future<void> main() async {
-  var path = '/home/dreamploy/Projects/Temp/read_text_from_file/assets/hi.txt';
+  var source =
+      '/home/dreamploy/Projects/Temp/read_text_from_file/assets/medical_suply.txt';
   // var destination = '/home/dreamploy/Projects/Temp/read_text_from_file/assets/hi2.txt';
 
-  var file = File(path);
+  var file = File(source);
 
   var lineSplitter =
       file.openRead().transform(utf8.decoder).transform(LineSplitter());
 
   await lineSplitter.forEach(
     (line) async {
-      // await Future.delayed(Duration.zero);
       print('"' + line + '"' + ',');
       // await File(destination).writeAsString(line, mode: FileMode.append);
     },
   );
-
-  // await Future.delayed(Duration.zero);
 }
